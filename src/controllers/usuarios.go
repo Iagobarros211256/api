8ab60cd6af597b2917b3e53b348a77c223a2e96a
Iagobarros211256 <iagobarrosgomes2112@gmail.com>
2025-10-07 03:19:51 +0000

    devbook estrutural basica

---
+++ src/controllers/usuarios.go
@@ -0,0 +1,47 @@
+package controllers
+
+import (
+	"api/src/banco"
+	"api/src/modelos"
+	"api/src/repositorios"
+	"encoding/json"
+	"io"
+	"log"
+	"net/http"
+)
+
+func CriarUsuario(w http.ResponseWriter, r *http.Request) {
+	corpoRequest, erro := io.ReadAll(r.Body)
+	if erro != nil {
+		log.Fatal(erro)
+	}
+
+	var usuario modelos.Usuario
+	if erro := json.Unmarshal(corpoRequest, &usuario); erro != nil {
+		log.Fatal(erro)
+	}
+
+	db, erro := banco.Conectar()
+	if erro != nil {
+		log.Fatal(erro)
+	}
+
+	repositorio := repositorios.NovoRepositorioDeUsuarios(db)
+	repositorio.Criar(usuario)
+}
+
+func BuscarUsuarios(w http.ResponseWriter, r *http.Request) {
+	w.Write([]byte("Buscando Usuarios"))
+}
+
+func BuscarUsuario(w http.ResponseWriter, r *http.Request) {
+	w.Write([]byte("Buscando Usuario"))
+}
+
+func AtualizarUsuario(w http.ResponseWriter, r *http.Request) {
+	w.Write([]byte("Atualizando Usuario"))
+}
+
+func DeletarUsuario(w http.ResponseWriter, r *http.Request) {
+	w.Write([]byte("Deletando Usuario"))
+}
