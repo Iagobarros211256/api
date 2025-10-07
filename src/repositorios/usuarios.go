b317bc7329e911c89507a56b085d2e760345cf04
Iagobarros211256 <iagobarrosgomes2112@gmail.com>
2025-10-07 03:55:38 +0000

    creaste function

--- src/repositorios/usuarios.go
+++ src/repositorios/usuarios.go
@@ -2,6 +2,7 @@ package repositorios
 
 import (
 	"api/src/modelos"
+
 	"database/sql"
 )
 
@@ -13,6 +14,22 @@ func NovoRepositorioDeUsuarios(db *sql.DB) *Usuarios {
 	return &Usuarios{db}
 }
 
-func (u Usuarios) Criar(usuario modelos.Usuario) (uint64, error) {
-	return 0, nil
+func (repositorios Usuarios) Criar(usuario modelos.Usuario) (uint64, error) {
+	statement, erro := repositorio.db.Prepare("insert into usuarios(nome, nick, email, senha) values(?, ?, ?, ?)")
+	if erro != nil {
+		return 0, erro
+	}
+	defer statement.Close()
+
+	resultado, erro := statement.Exec(usuario.Nome, usuario.Nick, usuario.Email, usuario.Senha)
+	if erro != nil {
+		return 0, erro
+	}
+	ultimoIdInserido, erro := resultado.LastInsertId()
+	if erro != nil {
+		return 0, erro
+	}
+
+	return uint64(ultimoIdInserido), nil
+
 }
