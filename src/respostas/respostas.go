a7a5b36fae334e4e8c0ec015fa44d1084d6e85f6
Iagobarros211256 <iagobarrosgomes2112@gmail.com>
2025-10-10 23:02:05 +0000

    fiished atualizar usuario function

--- src/respostas/respostas.go
+++ src/respostas/respostas.go
@@ -2,13 +2,18 @@ package respostas
 
 import (
 	"encoding/json"
+	"log"
 	"net/http"
 )
 
 func JSON(w http.ResponseWriter, statusCode int, dados interface{}) {
+	w.Header().Set("Content-Type", "application/json")
 	w.WriteHeader(statusCode)
 
-	if erro := json.NewEncoder(w).Encode(dados); erro != nil {
+	if dados != nil {
+		if erro := json.NewEncoder(w).Encode(dados); erro != nil {
+			log.Fatal(erro)
+		}
 
 	}
 }
