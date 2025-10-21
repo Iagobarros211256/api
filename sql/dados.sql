c021b13bcc631dbe77a43cf4e382f8889ce59227
Iagobarros211256 <iagobarrosgomes2112@gmail.com>
2025-10-21 14:28:48 +0000

    creating tabela de seguidores

---
+++ sql/dados.sql
@@ -0,0 +1,5 @@
+insert into usuarios (nome, nick, email, senha)
+values
+("Usuario 1", "usuario_1", "usuario1@gmail.com"),
+(),
+();
--- src/router/rotas/usuarios.go
+++ src/router/rotas/usuarios.go
@@ -24,20 +24,20 @@ var rotasUsuarios = []Rota{
 		URI:                "/usuarios/{usuarioId}",
 		Metodo:             http.MethodGet,
 		Funcao:             controllers.BuscarUsuario,
-		RequerAutenticacao: false,
+		RequerAutenticacao: true,
 	},
 
 	{
 		URI:                "/usuarios/{usuarioId}",
 		Metodo:             http.MethodPut,
 		Funcao:             controllers.AtualizarUsuario,
-		RequerAutenticacao: false,
+		RequerAutenticacao: true,
 	},
 
 	{
 		URI:                "/usuarios/{usuarioId}",
 		Metodo:             http.MethodDelete,
 		Funcao:             controllers.DeletarUsuario,
-		RequerAutenticacao: false,
+		RequerAutenticacao: true,
 	},
 }
