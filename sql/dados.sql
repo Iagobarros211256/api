de4c0dbe40e41f73a31d07f7a3799d9823d1665f
Iagobarros211256 <iagobarrosgomes2112@gmail.com>
2025-10-21 14:56:13 +0000

    testing dados table and implementing seguidores logic

--- sql/dados.sql
+++ sql/dados.sql
@@ -1,5 +1,11 @@
 insert into usuarios (nome, nick, email, senha)
 values
-("Usuario 1", "usuario_1", "usuario1@gmail.com"),
-(),
-();
+("Usuario 1", "usuario_1", "usuario1@gmail.com",  $2a$10$B3OOBT/N2tviWYzTGDMb.OPax3SzR3F4mIV5A6pz47dUauOfy.HxW ),
+("Usuario 2", "usuario_2", "usuario2@gmail.com",  $2a$10$B3OOBT/N2tviWYzTGDMb.OPax3SzR3F4mIV5A6pz47dUauOfy.HxW ),
+("Usuario 3", "usuario_3", "usuario3@gmail.com",  $2a$10$B3OOBT/N2tviWYzTGDMb.OPax3SzR3F4mIV5A6pz47dUauOfy.HxW );
+
+insert into seguidores(usuario_id, seguidor_id)
+values
+(1, 2),
+(3, 1),
+(1, 3);
