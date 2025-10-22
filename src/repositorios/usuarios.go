a636436abbff8557b3fefebc1ea86b65ef0c3b32
Iagobarros211256 <iagobarrosgomes2112@gmail.com>
2025-10-22 19:08:22 +0000

    finished seguir suario logic

--- src/repositorios/usuarios.go
+++ src/repositorios/usuarios.go
@@ -140,3 +140,18 @@ func (repositorio Usuarios) BuscarPorEmail(email string) (modelos.Usuario, error
 
 	return usuario, nil
 }
+
+func (repositorio Usuarios) Seguir(usuarioID, seguiddorID uint64) error {
+	statement, erro := repositorio.db.Prepare("insert ignore into seguidores (usuario_id, seguidor_id)  values (?, ?)")
+	if erro != nil {
+		return erro
+	}
+	defer statement.Close()
+
+	if _, erro = statement.Exec(usuarioID, seguiddorID); erro != nil {
+		return erro
+	}
+
+	return nil
+
+}
