7b2b9996807bff85d5d338c6f45ae6efb9f282c2
Iagobarros211256 <iagobarrosgomes2112@gmail.com>
2025-11-01 21:25:19 +0000

    finished deletar publicacao logic

--- src/repositorios/publicacoes.go
+++ src/repositorios/publicacoes.go
@@ -118,3 +118,19 @@ func (respositorio Publicacoes) Atualizar(publicacaoID uint64, publicacao modelo
 
 	return nil
 }
+
+// exclui uma publi do database
+func (repositorio Publicacoes) Deletar(publicacaoID uint64) error {
+	statement, erro := repositorio.db.Prepare("delete from publicacoes where id = ?")
+	if erro != nil {
+		return erro
+	}
+
+	defer statement.Close()
+
+	if _, erro = statement.Exec(publicacaoID); erro != nil {
+		return erro
+	}
+
+	return nil
+}
